807a75b882ac3bc29de37330935b44a4ff1e470d
BaeSooHyo <paola.bailleux@edu.univ-fcomte.fr>
2017-05-04 09:10:38 +0200

    Add files via upload

--- php/rendezvous.php
+++ php/rendezvous.php
@@ -1,8 +1,7 @@
 <?php
 
-//TODO gérer la connexion (mail présent dans la base de données)
 /**
-*
+* 
 *Page d'identification
 */
 // Bufferisation des sorties
@@ -10,6 +9,9 @@ ob_start();
 
 // Inclusion de la bibliothéque
 include('bibli_24sur7.php');
+
+session_start();
+fd_bd_connexion();
 
 	//_______________________________________________________________
 	/**
@@ -19,6 +21,8 @@ include('bibli_24sur7.php');
 	*/
 	function pbl_verif_rdv()
 	{
+
+
 		//On fait les v&eacute;rifications des donn&eacute;es
 		$er = array();
 		$Libelle = $_POST['txtLibelle'];
@@ -32,72 +36,107 @@ include('bibli_24sur7.php');
 		$HFinM = $_POST['selHFin_M'];
 		$EvJournee = $_POST['chkJournee'];
 
-		//Date
+        //Journée entière présente ?
+        if ($EvJournee==1)
+        {
+            $HDebutH = 0;
+            $HDebutM = 0;
+            $HFinH = 23;
+            $HFinM = 59;
+        }
+
+
+        //Date
 		if ( date("j", mktime(0, 0, 0, $dateM, -1, $dateY)) < $dateD )
 		{
 			array_push( $er, "La date n'est pas valide <br>");
 		}
-
+		
 		//Libelle
-		if ( (strlen($Libelle)) == 0)
+		if ( (strlen($Libelle)) == 0) 
 		{
 			array_push($er, "Le libell&eacute; est obligatoire<br>");
 		}
 
-		//On v&eacute;rifie la dur&eacute;e du rendez-vous
-		if ( ($HDebutH > $HFinM) || ( ($HDebutH == $HFinM) && ($HFinM < $HDebutM) ) )
+		$testDuree = (((int)$HFinM)+(((int)$HFinH)*60))-(((int)$HDebutM)+(((int)$HDebutH)*60)) ;
+
+		//On v&eacute;rifie la dur&eacute;e du rendez-vous 
+		if ( $testDuree < 15 )
 		{
 			array_push($er, "le rendez-vous doit durer au moins 15 minutes<br>");
 		}
-		if ( ($HDebutH == $HFinM) && (($HFinM - $HDebutM) < 15) )
-		{
-			array_push($er, "le rendez-vous doit durer au moins 15 minutes<br>");
-		}
-
-		/*$bd = fd_db_connexion();
-
-		//On v&eacute;rifie si le rendez vous n'en chevauche pas un d&eacute;j&agrave; pr&eacute;sent
-		$sql = 'SELECT  *
-			FROM rendezvous
-			WHERE rdvIDUtilisateur = ' . '"' . (mysqli_real_escape_string($GLOBALS['bd'], $_SESSION['utiID'])) . '"';
-
-		$res = mysqli_query($GLOBALS['bd'], $sql) OR fd_bd_erreur($sql);
 
 
-		$rdvHeureFinH = (int) substr( fd_heure_claire($test['rdvHeureFin']), 0, 1);
-		$rdvHeureFinM = (int) substr( fd_heure_claire($test['rdvHeureFin']), 3, 4);
-		$rdvHeureDebutH = (int) substr( fd_heure_claire($test['rdvHeureDebut']), 0, 1);
-		$rdvHeureDebutM = (int) substr( fd_heure_claire($test['rdvHeureDebut']), 3, 4);
+        $id = $_SESSION['utiID'];
 
-		while ($test = mysqli_fetch_assoc($res) )
-		{
-			if( ( ($HDebutH > $rdvHeureDebutH) && ($HDebutH < $rdvHeureFinH) ) || ( ($HFinH >= $rdvHeureDebutH) && ($HFinH <= $rdvHeureFinH) ) )
-			{
-				array_push($er, "le rendez-vous ne doit pas chevaucher un autre rendez-vous<br>");
-			}
-			if ( ($HDebutH = $rdvHeureDebutH) && ($HDebutM > $rdvHeureDebutM) )
-			{
-				array_push($er, "le rendez-vous ne doit pas chevaucher un autre rendez-vous<br>");
-			}
-			if ( ($HFinH = $rdvHeureFintH) )
-			{
-				array_push($er, "le rendez-vous ne doit pas chevaucher un autre rendez-vous<br>");
-			}
+        //On v&eacute;rifie si le rendez vous n'en chevauche pas un d&eacute;j&agrave; pr&eacute;sent
+        $sql = "SELECT *
+            FROM rendezvous
+            WHERE rdvIDUtilisateur = $id";
 
-		}	*/
+        $res = mysqli_query($GLOBALS['bd'], $sql) OR fd_bd_erreur($sql);
+
+        $testChev =0;
+
+        //On vérifie pour chaque rendez-vous déjà présent si tout est en ordre
+        while ($test = mysqli_fetch_assoc($res) ) {
+
+
+            $rdvHeureFinM = (int)substr(fd_heure_claire($test['rdvHeureFin']), -1, 2);
+            $rdvHeureFinH = (((int)fd_heure_claire($test['rdvHeureFin'])) -$rdvHeureFinM);
+            $hFin2=($rdvHeureFinH*60)+($rdvHeureFinM);
+            $rdvHeureDebutM = (int)substr(fd_heure_claire($test['rdvHeureDebut']), -1, 2);
+            $rdvHeureDebutH = (((int)fd_heure_claire($test['rdvHeureDebut'])) -$rdvHeureDebutM);
+            $hDeb2=($rdvHeureDebutH*60)+($rdvHeureDebutM);
+
+           $hFin = (((int)$HFinM)+(((int)$HFinH)*60));
+           $hDeb =(((int)$HDebutM)+(((int)$HDebutH)*60));
+
+			echo ' hFin ' . $hFin . ' ';
+            echo ' hFin2 ' . $hFin2 . ' ';
+            echo ' hDeb ' . $hDeb . ' ';
+            echo ' hDeb2 ' . $hDeb2 . ' ';
+
+            if ( (($hDeb<$hFin2) && ($hDeb>$hDeb2)) || (($hFin<$hFin2) && ($hFin>$hDeb2)) )
+            {
+               $testChev=1;
+            }
+            if  (($hDeb<$hDeb2) && ($hFin>$hFin2))
+            {
+                $testChev=1;
+            }
+
+            mysqli_free_result($test);
+
+        }
+
+        if ( $testChev == 1 )
+        {
+            array_push($er, "le rendez-vous ne doit pas chevaucher un autre rendez-vous<br>");
+        }
 
 		return $er;
 	}
 
+	//TODO si modification ou ajout d'un rendezvous
+	if ( 0 === '0')
+	{
+		$titre = 'Modification';
+	}
+	else
+	{
+		$titre = 'Nouvelle saisie';
+	}
+
 	$erreurs = array();
-
-
-	if($_POST['btnIdentifier'])
+	
+	
+	if($_POST['btnAjouter'])
 	{
 		$Libelle = $_POST['txtLibelle'];
-		$dateD = $_POST['selDate_d'];
+		$dateD = $_POST['selDate_j'];
 		$dateM = $_POST['selDate_m'];
-		$dateY = $_POST['selDate_y'];
+		$dateY = $_POST['selDate_a'];
 		$Categorie = $_POST['selCat'];
 		$HDebutH = $_POST['selHDebut_H'];
 		$HDebutM = $_POST['selHDebut_M'];
@@ -106,58 +145,110 @@ include('bibli_24sur7.php');
 		$EvJournee = $_POST['chkJournee'];
 		$erreurs = pbl_verif_rdv();
 
-		//L'utilisateur est connect&eacute;
-		if ( $erreurs == NULL)
+		//Journée entière présente ?
+		if ($EvJournee==='journee')
 		{
-			//ajout d'un rendez-vous &agrave; la base de donn&eacute;es
-			echo 'Ajout possible<br>';
+            $HDebutH = 0;
+            $HDebutM = 0;
+            $HFinH = 23;
+            $HFinM = 59;
 		}
 
 
+		if ( $erreurs == NULL) 
+		{
+			$dateF=($dateY . $dateM . $dateD);
+			$dateF=(int)$dateF;
+			$hD = (int)($HDebutH . $HDebutM);
+			$hF = (int)($HFinH . $HFinM);
+			$Lib= mysqli_real_escape_string($GLOBALS['bd'], $Libelle);
+
+			//ajout d'un rendez-vous a la base de donnees
+            $S = "INSERT INTO rendezvous SET
+			rdvDate = $dateF,
+			rdvHeureDebut = $hD,
+			rdvHeureFin = $hF,
+			rdvLibelle = $Lib,
+			rdvIDCategorie = $Categorie,
+			rdvIDUtilisateur = ". $_SESSION['utiID'];
+
+            $R = mysqli_query($GLOBALS['bd'], $S) or fd_bd_erreur($S);
+
+		}
+
+		
 	}
-	else
+	else 
 	{
 		$Libelle ='';
 		$dateD = '21';
 		$dateM = '10';
 		$dateY = '2017';
-		$Categorie = '';
 		$HDebutH = '0';
 		$HDebutM ='0';
 		$HFinH = '0';
 		$HFinM = '0';
-		$EvJournee = '';
+		$EvJournee = 0;
 	}
 
-// Si on est encore là, c'est que l'utilisateur est bien authentifié.
-fd_html_head(APP_NOM_APPLICATION' | RendezVous');
+
+//Affichage de la page html
+fd_html_head('RendezVous | 24sur7');
 
 //On affiche le bandeau avec les onglets non s&eacute;lectionn&eacute;s
 fd_html_bandeau('x');
 
 
+
+
 //On affiche la phrase avant le formulaire
-echo '<main id="bcContenu">',
-		'<h1>Nouvelle saisie</h1>',
-		'<div class="aligncenter">';
+echo '<main>';
+echo '<section id="bcContenu"><div class="aligncenter"><section id="bcGauche">';
+fd_html_calendrier();
+//TODO Catégorie
+echo '</section>';
+
+
+$r = '<select class="bcListe" id="categorie" name="categorie">';
+//On recupere les categories de l'utilisateur
+$sql = "SELECT *
+            FROM categorie
+            WHERE catIDUtilisateur = 1" ;
+
+$res = mysqli_query($GLOBALS['bd'], $sql) OR fd_bd_erreur($sql);
+
+while ($test = mysqli_fetch_assoc($res) ) {
+    if ($test['catID'] == NULL)
+    {
+        $r .='Acune categorie';
+	} else
+	{
+        $r .= '<option value="' . $test['catID'] . '">' . (htmlentities(($test['catNom']), ENT_QUOTES, 'UTF-8')) . '</option>';
+	}
+	mysqli_free_result($test);
+}
+$r .= '</select>';
+
 
 	//On affiche le formulaire
-	echo		'<form method="POST" action="rendezvous.php">',
+	echo	'<section id="bcCentreRDV">',
+    			'<h1 id="titreRDV">' . $titre . '</h1>',
+				'<form method="POST" action="rendezvous.php">',
 				'<div class="formulaire">',
 				'<table border="0" cellpadding="4" cellspacing="0">',
-					fd_form_ligne('Libell&eacute; :', fd_form_input(APP_Z_TEXT, 'txtLibelle', (htmlentities(($Libelle), ENT_COMPAT, 'ISO-8859-1')) , '40')),
+					fd_form_ligne('Libell&eacute; :', fd_form_input(APP_Z_TEXT, 'txtLibelle', (htmlentities(($Libelle), ENT_COMPAT, 'ISO-8859-1')) , '20')),
 					fd_form_ligne('Date :',fd_form_date('selDate',$dateD ,$dateM,$dateY )),
-					fd_form_ligne('Cat&eacute;gorie :', 'a faire' ),
-					fd_form_ligne('Horaire d&eacute;but :',pb_form_heure('selHeure',$HDebutH,$HDebutM)),
-					fd_form_ligne('Horaire fin :',pb_form_heure('selHeure',$HFinH,$HFinM)),
-					fd_form_ligne('Ou', fd_form_input(APP_Z_CHECKBOX, 'btnajouter', 'chkJournee').'Ev&eacute;nement sur une journ&eacute;e'),
-					fd_form_ligne( fd_form_input(APP_Z_SUBMIT, 'btnajouter', 'Ajouter'), fd_form_input(APP_Z_RESET, 'btnAnnuler', 'Annuler')),
+					fd_form_ligne('Cat&eacute;gorie :',  $r),
+					fd_form_ligne('Horaire d&eacute;but :',pb_form_heure('selHDebut',$HDebutH,$HDebutM)),
+					fd_form_ligne('Horaire fin :',pb_form_heure('selHFin',$HFinH,$HFinM)),
+					fd_form_ligne('Ou', fd_form_input(APP_Z_CHECKBOX, 'chkJournee', 'journee', $EvJournee). '<label for="chkJournee">Ev&eacute;nement sur une journ&eacute;e</label>'),
+					fd_form_ligne( fd_form_input(APP_Z_SUBMIT, 'btnAjouter', 'Ajouter'), fd_form_input(APP_Z_RESET, 'btnAnnuler', 'Annuler')),
 				'</table>',
 				'</div>',
 			'</form>';
-
-	//Si il y a des erreurs, on les affiches apr�s le formulaire
-	if ( $erreurs != NULL)
+			
+	//Si il y a des erreurs, on les affiches apres le formulaire
+	if ( $erreurs != NULL) 
 	{
 		//Affichage du d&eacute;but de la page html
 		$tailleA = count($erreurs);
@@ -169,12 +260,14 @@ echo '<main id="bcContenu">',
 	}
 
 
-//On affiche les phrases apr�s le formulaire
-	echo 	 '<br><p><a href="agenda.php">', "Retour &agrave; l'agenda</a></p>",
-		'</div>',
-	'</main>';
-
+	//On affiche les phrases apr�s le formulaire
+	echo 	 '</section> <br><p id="lienRDV"><a href="agenda.php">', "Retour &agrave; l'agenda</a></p>",
+		'</section> </div>',
+	'</main>';	
+	
 //On affiche le pied de page
 fd_html_pied();
+
+mysqli_close($GLOBALS['bd']);
 
 ?>
